a98399f2c6518379d595bc2b189cdeb488b80bfc
Johannes Drechsler <JohannesDrechslerUS@gmail.com>
2018-10-30 10:11:17 -0400

    Initial commit

---
+++ src/App.vue
@@ -0,0 +1,41 @@
+<template>
+  <div id="app">
+    <vue-dropzone 
+      ref="dropzone" 
+      id="drop1" 
+      :options="dropOptions"
+      @vdropzone-complete="afterComplete"
+    ></vue-dropzone>
+    <button @click="removeAllFiles">Remove All Files</button>
+  </div>
+</template>
+
+<script>
+import vueDropzone from "vue2-dropzone";
+
+export default {
+  data: () => ({
+    dropOptions: {
+      url: "https://httpbin.org/post",
+      maxFilesize: 2, // MB
+      maxFiles: 4,
+      chunking: true,
+      chunkSize: 500, // Bytes
+      thumbnailWidth: 150, // px
+      thumbnailHeight: 150,
+      addRemoveLinks: true
+    }
+  }),
+  components: {
+    vueDropzone
+  },
+  methods: {
+    afterComplete(file) {
+      console.log(file);
+    },
+    removeAllFiles() {
+      this.$refs.dropzone.removeAllFiles();
+    }
+  }
+};
+</script>
---
+++ src/main.js
@@ -0,0 +1,10 @@
+import Vue from "vue";
+import App from "./App";
+import "vue2-dropzone/dist/vue2Dropzone.css";
+
+/* eslint-disable no-new */
+new Vue({
+  el: "#app",
+  components: { App },
+  template: "<App/>"
+});
